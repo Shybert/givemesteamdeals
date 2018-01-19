16a47e2fe432ef386b777aaab6be0b076023b898
Shybert <sivert.olstad@gmail.com>
2018-01-19 15:07:15 +0100

    Getting Steam Id works in a basic form

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 const htmlparser = require("htmlparser2");
 const request = require("request");
 const EventEmitter = require("events");
+const mysql = require("mysql");
 
 // Setting up event emitters
 class GetSteamIdsEmitter extends EventEmitter {}
 const getSteamIdsEmitter = new GetSteamIdsEmitter();
 
+// Setting up MySQL
+const connection = mysql.createConnection({
+    user: "root",
+    password: "3oFkAlziyG",
+    database: "games_list_test",
+});
+connection.connect((err) => {
+    if (err) {
+        return console.log(`Error while connecting to MySQL: ${err}`);
+    }
+    console.log("Connected to the MySQL server.");
+});
+
 // The initial steam link, used to find the number of pages to go through
 const initialSteamLink = "http://store.steampowered.com/search/results?sort_by=Released_DESC&category1=998&cc=en&v5=1&page=1";
+
 let numberOfPages;
+let currentPage;
 
 request(initialSteamLink, (err, res, body) => {
     if (err) {
@@ -21,7 +37,7 @@ request(initialSteamLink, (err, res, body) => {
         ontext: (text) => {
             if (Number(text) > 750) {
                 numberOfPages = Number(text);
-                getSteamIdsEmitter.emit("pagenum");
+                getSteamIdsEmitter.emit("pageNumEvent");
             }
         },
     });
@@ -29,8 +45,51 @@ request(initialSteamLink, (err, res, body) => {
     parser.end();
 });
 
-getSteamIdsEmitter.on("pagenum", () => {
+getSteamIdsEmitter.on("pageNumEvent", () => {
     console.log(`The number of pages to go through is: ${numberOfPages}`);
 
-    
+    // Sets up the page to start going through
+    currentPage = 1;
+    findSteamIds();
 });
+
+getSteamIdsEmitter.on("nextSearchPageEvent", () => {
+    if (currentPage <= numberOfPages) {
+        findSteamIds();
+    } else {
+        console.log("Finished going through Steam search pages");
+    }
+});
+
+// The function, called multiple times, to find Steam IDs
+function findSteamIds() {
+    const steamLink = "http://store.steampowered.com/search/results?sort_by=Released_DESC&category1=998&cc=en&v5=1&page=" + currentPage.toString();
+
+    // Request the appropriate page
+    request(steamLink, (err, res, body) => {
+        if (err) {
+            return console.log(`Error while requesting steam page #${currentPage}: ${err}`);
+        }
+        console.log(`Steam page #${currentPage} response code: ${res.statusCode}`);
+
+        const parser = new htmlparser.Parser({
+            onopentag: (name, attribs) => {
+                if (name === "a" && attribs.class === "search_result_row ds_collapse_flag") {
+                    if (attribs["data-ds-packageid"] !== undefined) {
+                        connection.query(`INSERT INTO games(steam_id) VALUES(${attribs["data-ds-packageid"]})`, (err) => {
+                        });
+                        console.log(`Package ID: ${attribs["data-ds-packageid"]}`);
+                    } else {
+                        connection.query(`INSERT INTO games(steam_id) VALUES(${attribs["data-ds-appid"]})`, (err) => {
+                        });
+                        console.log(`App ID: ${attribs["data-ds-appid"]}`);
+                    }
+                }
+            },
+        });
+        parser.write(body);
+        parser.end();
+        currentPage += 1;
+        getSteamIdsEmitter.emit("nextSearchPageEvent");
+    });
+}
