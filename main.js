b74c81a91379a49565dd1e680e38de000fe964a6
Shybert <sivert.olstad@gmail.com>
2018-02-06 18:00:28 +0100

    Re-enabled scheduling

--- main.js
+++ main.js
@@ -4,42 +4,42 @@ const getCheapSharkData = require("./getCheapSharkData");
 const schedule = require("node-schedule");
 
 // Scheduled
-// const i = schedule.scheduleJob("* 0 * * *", () => {
-//     console.log("Scheduling job");
-//     getSteamSpyData((err, results) => {
-//         if (results === "completed") {
-//             console.log("\nFunction getSteamSpyData has completed");
-//             console.log("Executing function getPackages()");
-//             getPackages((pacakgeErr, packageResults) => {
-//                 if (packageResults === "completed") {
-//                     console.log("\nFunction getPackages has completed");
-//                     console.log("Executing function getCheapSharkData()");
-//                     getCheapSharkData((cheapSharkErr, cheapSharkResults) => {
-//                         if (cheapSharkResults === "completed") {
-//                             console.log("\nFunction getCheapSharkData() has completed");
-//                         }
-//                     });
-//                 }
-//             });
-//         }
-//     });
-// });
+const i = schedule.scheduleJob("* 0 * * *", () => {
+    console.log("Scheduling job");
+    getSteamSpyData((err, results) => {
+        if (results === "completed") {
+            console.log("\nFunction getSteamSpyData has completed");
+            console.log("Executing function getPackages()");
+            getPackages((pacakgeErr, packageResults) => {
+                if (packageResults === "completed") {
+                    console.log("\nFunction getPackages has completed");
+                    console.log("Executing function getCheapSharkData()");
+                    getCheapSharkData((cheapSharkErr, cheapSharkResults) => {
+                        if (cheapSharkResults === "completed") {
+                            console.log("\nFunction getCheapSharkData() has completed");
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
 
 // No schedule
-getSteamSpyData((err, results) => {
-    if (results === "completed") {
-        console.log("\nFunction getSteamSpyData has completed");
-        console.log("Executing function getPackages()");
-        getPackages((pacakgeErr, packageResults) => {
-            if (packageResults === "completed") {
-                console.log("\nFunction getPackages has completed");
-                console.log("Executing function getCheapSharkData()");
-                getCheapSharkData((cheapSharkErr, cheapSharkResults) => {
-                    if (cheapSharkResults === "completed") {
-                        console.log("\nFunction getCheapSharkData() has completed");
-                    }
-                });
-            }
-        });
-    }
-});
+// getSteamSpyData((err, results) => {
+//     if (results === "completed") {
+//         console.log("\nFunction getSteamSpyData has completed");
+//         console.log("Executing function getPackages()");
+//         getPackages((pacakgeErr, packageResults) => {
+//             if (packageResults === "completed") {
+//                 console.log("\nFunction getPackages has completed");
+//                 console.log("Executing function getCheapSharkData()");
+//                 getCheapSharkData((cheapSharkErr, cheapSharkResults) => {
+//                     if (cheapSharkResults === "completed") {
+//                         console.log("\nFunction getCheapSharkData() has completed");
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// });
