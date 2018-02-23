9cbc1ad5eb25bee27d48f358cba6e916b248231d
Shybert <sivert.olstad@gmail.com>
2018-02-23 07:15:54 +0100

    Syncing, async/await work started

--- tests/test2.js
+++ tests/test2.js
@@ -1,1 +1,9 @@
 const db = require("../modules/db2");
+
+db.getBasicData(10).then((data) => {
+    console.log(`Basic data: ${JSON.stringify(data)}`);
+});
+
+db.getDevOrPub(10, "publisher").then((data) => {
+    console.log(`Dev/Pub data: ${JSON.stringify(data)}`);
+});
